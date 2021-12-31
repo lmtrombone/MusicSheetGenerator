58edd17d96a69513b796bdadb3884da28981b312
Leonard <lmtrombone@gmail.com>
2021-12-30 22:14:10 -0800

    remove unnecessary interations to clear a tmp array

--- src/main/java/FastFourierTransform.java
+++ src/main/java/FastFourierTransform.java
@@ -1,16 +1,19 @@
+import java.util.Stack;
+
 public class FastFourierTransform {
   public static Complex[] computeFft(Complex[] cps) {
-    return computeFft(cps, new Complex[cps.length], cps.length, 0, 1);
+    return computeFft(cps, new Stack<>(), cps.length, 0, 1);
   }
 
-  public static Complex[] computeFft(Complex[] cps, Complex[] tmpArr, int arrSize, int startPos, int stepSize) {
+  public static Complex[] computeFft(Complex[] cps, Stack<ImmutablePair<Integer, Complex>> tmpStack, int arrSize,
+                                     int startPos, int stepSize) {
     if (arrSize == 1) {
       return cps;
     }
 
     int splitArrSize = arrSize / 2;
-    computeFft(cps, tmpArr, splitArrSize, startPos, 2 * stepSize);
-    computeFft(cps, tmpArr, splitArrSize, startPos + stepSize, 2 * stepSize);
+    computeFft(cps, tmpStack, splitArrSize, startPos, 2 * stepSize);
+    computeFft(cps, tmpStack, splitArrSize, startPos + stepSize, 2 * stepSize);
 
     int evenArrPos = startPos;
     int oddArrPos = startPos + stepSize;
@@ -22,8 +25,8 @@ public class FastFourierTransform {
       Complex result1 = Complex.add(cps[evenArrPos], Complex.multiply(wk, cps[oddArrPos]));
       Complex result2 = Complex.subtract(cps[evenArrPos], Complex.multiply(wk, cps[oddArrPos]));
 
-      tmpArr[kthPos] = result1;
-      tmpArr[kn2thPos] = result2;
+      tmpStack.add(new ImmutablePair<>(kthPos, result1));
+      tmpStack.add(new ImmutablePair<>(kn2thPos, result2));
 
       evenArrPos += 2 * stepSize;
       oddArrPos += 2 * stepSize;
@@ -31,12 +34,9 @@ public class FastFourierTransform {
       kn2thPos += stepSize;
     }
 
-    for (int i = 0; i < cps.length; i++) {
-      if (tmpArr[i] != null) {
-        cps[i] = tmpArr[i];
-      }
-
-      tmpArr[i] = null;
+    while (!tmpStack.isEmpty()) {
+      ImmutablePair<Integer, Complex> pair = tmpStack.pop();
+      cps[pair.getKey()] = pair.getValue();
     }
 
     return cps;
---
+++ src/main/java/ImmutablePair.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public class ImmutablePair<K, V> {
+  private final K key;
+  private final V value;
+
+  public ImmutablePair(K key, V value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ImmutablePair<?, ?> that = (ImmutablePair<?, ?>) o;
+    return Objects.equals(key, that.key) && Objects.equals(value, that.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(key, value);
+  }
+}
