ef6d297b75fa2149c49ef36de115a2b8aad2ef93
Leonard <lmtrombone@gmail.com>
2022-01-04 22:48:38 -0800

    fix test names

--- src/test/java/NotesTest.java
+++ src/test/java/NotesTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 
 public class NotesTest {
   @Test
-  public void testGetNoteForValidFrequency() {
+  public void testFindKeyNumForValidFrequency() {
     int actualKeyNum = Notes.findKeyNumFromFrequency(257.8125);
     int expectedKeyNum = 40;
 
@@ -11,7 +11,7 @@ public class NotesTest {
   }
 
   @Test
-  public void testGetNoteForInvalidFrequency() {
+  public void testKeyNumForInvalidFrequency() {
     int actualKeyNum = Notes.findKeyNumFromFrequency(255.0);
     int expectedKeyNum = -1;
 
